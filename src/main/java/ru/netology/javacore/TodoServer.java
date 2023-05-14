8e234a02df9f6dfad48feac89c06ac85c0a4c0af
ConstDet <constx7@gmail.com>
2023-05-14 13:22:15 +0300

    Initiale commit Task Manager. GO!

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -8,14 +8,24 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class TodoServer {
-    //...
+    private int port;
+    private Todos todos;
 
     public TodoServer(int port, Todos todos) {
-        //...
+        this.port = port;
+        this.todos = todos;
     }
 
     public void start() throws IOException {
         System.out.println("Starting server at " + port + "...");
-        //...
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (Socket socket = serverSocket.accept();//ждем подключения
+                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                    todos.addTask(in.readLine());
+                }
+            }
+        }
     }
 }
--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -1,13 +1,23 @@
 package ru.netology.javacore;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Todos {
-    //...
+    private Set<String> taskList;
+
+    public Todos() {
+        taskList = new TreeSet<>();
+    }
 
     public void addTask(String task) {
-        //...
+        if (task == null || task.equals("")) return;
+        Gson gson = new GsonBuilder().create();
+        String taskString = gson.toJson(task);
+
     }
 
     public void removeTask(String task) {
